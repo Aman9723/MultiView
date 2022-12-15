933c78b871633ca5f30d6b66a74773c03c88bc2a
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-12-15 20:38:26 +0530

    res

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import SpeechContextProvider from './contexts/SpeechContext';
 import { BrowserRouter } from 'react-router-dom';
@@ -17,7 +16,4 @@ root.render(
     </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
