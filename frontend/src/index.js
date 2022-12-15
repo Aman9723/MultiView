6c9f5e6b2d94e7fa7c24e75e73d81104cde209be
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-12-16 00:17:27 +0530

    files updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import SpeechContextProvider from './Contexts/SpeechContext';
+import SpeechContextProvider from './contexts/SpeechContext';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-        <SpeechContextProvider>
-            <ChakraProvider>
-                <App />
-            </ChakraProvider>
-        </SpeechContextProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <SpeechContextProvider>
+                <ChakraProvider>
+                    <App />
+                </ChakraProvider>
+            </SpeechContextProvider>
+        </BrowserRouter>
+    </Provider>
 );
-
-
