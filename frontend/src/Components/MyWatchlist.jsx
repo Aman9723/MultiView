37eecf137718c9fc086338fda4101ded3859e5e9
Aman Singh Chauhan <65977764+Aman9723@users.noreply.github.com>
2022-12-16 11:52:57 +0530

    Create MyWatchlist.jsx

---
+++ frontend/src/Components/MyWatchlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import ProfileNavbar from './MyProfile';
+
+const MyWatchlist = () => {
+    return <ProfileNavbar cur={'watchlist'} />;
+};
+
+export default MyWatchlist;
