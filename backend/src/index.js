9fbc7aabe9163b636d9d560600555586bc3d1e78
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-12-17 14:46:24 +0530

    watchlist server

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,16 +7,7 @@ const watchlistModel = require('./models/watchlist.model');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// request for adding watchlist in db
-app.post('/watchlist', async (req, res) => {
-    try {
-        const { image, email, title } = req.body;
-        await watchlistModel.create({ image, email, title });
-        return res.status(200).send('Added in watchlist');
-    } catch (e) {
-        return res.status(400).send(e.message);
-    }
-});
+const port = process.env.PORT;
 
 // request for showing all watchlist of a user
 app.get('/watchlist', async (req, res) => {
@@ -31,8 +22,30 @@ app.get('/watchlist', async (req, res) => {
     }
 });
 
+// request for adding watchlist in db
+app.post('/watchlist', async (req, res) => {
+    try {
+        const { image, email, title } = req.body;
+        await watchlistModel.create({ image, email, title });
+        return res.status(200).send('Added in watchlist');
+    } catch (e) {
+        return res.status(400).send(e.message);
+    }
+});
+
+// request for removing watchlist of a user
+app.delete('/watchlist', async (req, res) => {
+    try {
+        const { image } = req.body;
+        await watchlistModel.findOneAndDelete({ image });
+        return res.status(200).send('Deleted from watchlist');
+    } catch (e) {
+        return res.status(400).send(e.message);
+    }
+});
+
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    app.listen(8000, () => {
-        console.log(`Listening on http://localhost:8000`);
+    app.listen(port, () => {
+        console.log(`Listening on ${port}`);
     });
 });
